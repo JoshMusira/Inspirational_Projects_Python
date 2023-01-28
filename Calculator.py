af829368468712b4904bf853f04f5d967b5a92cb
Josh <joshuamusira38@gmail.com>
2023-01-28 18:38:12 +0300

    All Projects that I have learned from a tutor

---
+++ Calculator.py
@@ -0,0 +1,6 @@
+import tkinter
+from tkinter import *
+
+root = Tk()
+root.title("Simple Calculator")
+root.geometry("570*600+100+200")
---
+++ SpellingChecker.py
@@ -0,0 +1,12 @@
+import tkinter
+
+from tkinter import *
+from textblob import TextBlob
+
+root = Tk()
+root.title("Spelling Checker")
+root.geometry("700*400")
+root.config(background="#dae6f6")
+
+
+root.mainloop()
